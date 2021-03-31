381c50534256dcd4a8c0979e3de712641c7fb0c6
chulhee23 <chulhee2351@gmail.com>
2021-03-31 21:01:21 +0900

    marker 수정

--- src/capstone_LCH/src/subscriber.cpp
+++ src/capstone_LCH/src/subscriber.cpp
@@ -7,16 +7,22 @@ ros::Publisher marker_pub;
 void callback(const capstone_LCH::pointXY::ConstPtr& input)
 {
   visualization_msgs::Marker line_list;
+  
   line_list.header.frame_id = "/my_frame";
   line_list.header.stamp = ros::Time::now();
+  
   line_list.ns = "points_and_lines";
-  line_list.action = visualization_msgs::Marker::ADD;
-  line_list.pose.orientation.w = 1.0;
-  line_list.id = 2;
+  line_list.id = 0;
+  
   line_list.type = visualization_msgs::Marker::LINE_LIST;
-  line_list.scale.x = 0.1;
-  line_list.scale.r = 1.0;
-  line_list.scale.a = 1.0;
+  
+  line_list.action = visualization_msgs::Marker::ADD;
+  
+  line_list.scale.x = 1;
+  line_list.scale.y = 1;
+  line_list.scale.z = 1;
+  
+  line_list.pose.orientation.w = 1.0;
 
   for (int i = 0; i < 26; i++)
   {
@@ -27,7 +33,7 @@ void callback(const capstone_LCH::pointXY::ConstPtr& input)
     line_list.points.push_pack(p);
 
   }
-  
+
   marker_pub.publish(line_list);
 
 
